a6d5215e75627fe2da7a053a084e249b8d29e436
gilgamesh7 <felidaesrule@yahoo.com>
2022-06-07 21:42:51 +1200

    Create list, detail and add blog post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.title} | {self.author}"
+
+    def get_absolute_url(self):
+        # return reverse('blog:article-detail', args=(str(self.id)))
+        return reverse('blog:home')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+
+from blog.models import Post
 
 # Create your views here.
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'blog/article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'blog/add_post.html'
+    fields = '__all__'
